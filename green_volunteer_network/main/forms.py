1b9ce1fab084174cb209556b7a03a42ae4c3ae01
Gurpreet Kaur <kaur431@uwindsor.ca>
2024-07-18 18:06:20 -0400

    Add validations using form and enhanced the UI

--- green_volunteer_network/main/forms.py
+++ green_volunteer_network/main/forms.py
@@ -2,7 +2,17 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
+from django.contrib.auth.forms import AuthenticationForm
 
+class CustomLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+        label='Username'
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
+        label='Password'
+    )
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
--- green_volunteer_network/main/views.py
+++ green_volunteer_network/main/views.py
@@ -1,6 +1,81 @@
-from django.shortcuts import render
-from .models import Opportunity
+from datetime import timezone
+
+from django.contrib.auth import logout, login, authenticate
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.views.decorators.csrf import csrf_protect
+
+from .models import VolunteerOpportunity
+from .forms import UserRegisterForm
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .forms import ProfileUpdateForm
+from .models import Profile
 
 def home(request):
-    opportunities = Opportunity.objects.all()
+    opportunities = VolunteerOpportunity.objects.all()
     return render(request, 'main/home.html', {'opportunities': opportunities})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
+        else:
+            print(form.errors)
+    else:
+        form = UserRegisterForm()
+    return render(request, 'main/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    user_profile, created = Profile.objects.get_or_create(user=request.user)
+
+    if request.method == 'POST':
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=user_profile)
+        if p_form.is_valid():
+            print("Form is valid. Saving the profile photo.")
+            p_form.save()
+            return redirect('profile')  # Redirect to the same view after successful update
+        else:
+            print("Form is invalid.")
+    else:
+        p_form = ProfileUpdateForm(instance=user_profile)
+
+    context = {
+        'user_profile': user_profile,
+        'p_form': p_form,
+    }
+
+    return render(request, 'main/profile.html', context)
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    request.session['last_login'] = str(timezone.now())
+                    request.session.set_expiry(60)
+                    return redirect(reverse('home'))
+                else:
+                    return HttpResponse('Your account is disabled.')
+            else:
+                return HttpResponse('Invalid login details.')
+        else:
+            return render(request, 'registration/login.html', {'form': form})
+    else:
+        form = AuthenticationForm()
+        return render(request, 'registration/login.html', {'form': form})
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
